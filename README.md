f53593d8e22e9f70a1e360223ab73000996f6f13
Fernando Chicaiza <washopilot@yahoo.com>
2022-06-09 12:33:01 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Socket-Server
+
+Reconstruir modulos de Node
+
+```
+npm install
+```
+
+Generar el DIST
+
+```
+tsc -w
+```
+
+Levantar servidor
+
+```
+nodemon dist/
+node dist/
+```
---
+++ index.ts
@@ -0,0 +1,20 @@
+import Server from './classes/server';
+import { SERVER_PORT } from './global/enviroment';
+import { router } from './routes/router';
+import cors from 'cors';
+
+import bodyParser from 'body-parser';
+
+const server = new Server();
+
+// BodyParser
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
+
+// CORS
+server.app.use(cors({ origin: true, credentials: true }));
+
+// Rutas de servicios
+server.app.use('/', router);
+
+server.start(() => console.log(`Servidor corriendo en el puerto ${SERVER_PORT}`));
